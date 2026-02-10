66233777d48868412e0e8cf24391f369c3186a3d
aggarwalshardul <aggarwalshardul@gmail.com>
2026-02-10 09:31:11 +0530

    Time: 0 ms (100%), Space: 10.6 MB (18.89%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
@@ -0,0 +1,26 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+     int freq[1001] = {0};
+
+    // Count frequency of nums1
+    for (int i = 0; i < nums1Size; i++) {
+        freq[nums1[i]]++;
+    }
+
+    // Allocate max possible size
+    int* result = (int*)malloc(sizeof(int) * (nums1Size < nums2Size ? nums1Size : nums2Size));
+    int k = 0;
+
+    // Find intersection
+    for (int i = 0; i < nums2Size; i++) {
+        if (freq[nums2[i]] > 0) {
+            result[k++] = nums2[i];
+            freq[nums2[i]]--;
+        }
+    }
+
+    *returnSize = k;
+    return result;
+}
