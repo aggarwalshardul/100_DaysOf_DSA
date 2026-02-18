0352b7d0d81b6e408f7ca16a013a2527284a9a5d
aggarwalshardul <aggarwalshardul@gmail.com>
2026-02-18 21:40:31 +0530

    Time: 0 ms (100%), Space: 30.3 MB (64.75%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+       *returnSize = numsSize;
+    
+    int* answer = (int*)malloc(sizeof(int) * numsSize);
+    
+    // Step 1: Store left products
+    answer[0] = 1;
+    for(int i = 1; i < numsSize; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+    
+    // Step 2: Multiply with right products
+    int right = 1;
+    for(int i = numsSize - 1; i >= 0; i--) {
+        answer[i] = answer[i] * right;
+        right *= nums[i];
+    }
+    
+    return answer;
+}
