1cb8ad68c4234f867d9031ad3b8af4089cd87c97
aggarwalshardul <aggarwalshardul@gmail.com>
2026-02-26 14:26:43 +0530

    Time: 10 ms (74.94%), Space: 19.4 MB (68.61%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.c
+++ 0707-design-linked-list/0707-design-linked-list.c
@@ -1,3 +1,6 @@
+
+/** Delete the index-th node if valid. */
+
 #include <stdlib.h>
 
 typedef struct Node {
@@ -7,62 +10,62 @@ typedef struct Node {
 
 typedef struct {
     Node* head;
+    Node* tail;
     int size;
 } MyLinkedList;
 
 
-/** Initialize your data structure here. */
 MyLinkedList* myLinkedListCreate() {
     MyLinkedList* obj = (MyLinkedList*)malloc(sizeof(MyLinkedList));
     obj->head = NULL;
+    obj->tail = NULL;
     obj->size = 0;
     return obj;
 }
 
 
-/** Get the value of the index-th node in the linked list. */
 int myLinkedListGet(MyLinkedList* obj, int index) {
     if (index < 0 || index >= obj->size)
         return -1;
 
     Node* temp = obj->head;
-    for (int i = 0; i < index; i++)
+    while (index--)
         temp = temp->next;
 
     return temp->val;
 }
 
 
-/** Add a node of value val before the first element. */
 void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->val = val;
     newNode->next = obj->head;
+
     obj->head = newNode;
+
+    if (obj->size == 0)
+        obj->tail = newNode;
+
     obj->size++;
 }
 
 
-/** Append a node of value val at the last element. */
 void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->val = val;
     newNode->next = NULL;
 
-    if (obj->head == NULL) {
-        obj->head = newNode;
+    if (obj->size == 0) {
+        obj->head = obj->tail = newNode;
     } else {
-        Node* temp = obj->head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = newNode;
+        obj->tail->next = newNode;
+        obj->tail = newNode;
     }
 
     obj->size++;
 }
 
 
-/** Add a node before the index-th node. */
 void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
     if (index < 0 || index > obj->size)
         return;
@@ -72,20 +75,25 @@ void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
         return;
     }
 
+    if (index == obj->size) {
+        myLinkedListAddAtTail(obj, val);
+        return;
+    }
+
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->val = val;
 
     Node* temp = obj->head;
-    for (int i = 0; i < index - 1; i++)
+    while (--index)
         temp = temp->next;
 
     newNode->next = temp->next;
     temp->next = newNode;
+
     obj->size++;
 }
 
 
-/** Delete the index-th node if valid. */
 void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
     if (index < 0 || index >= obj->size)
         return;
@@ -94,30 +102,37 @@ void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
 
     if (index == 0) {
         obj->head = temp->next;
+        if (obj->size == 1)
+            obj->tail = NULL;
         free(temp);
     } else {
-        for (int i = 0; i < index - 1; i++)
+        while (--index)
             temp = temp->next;
 
-        Node* nodeToDelete = temp->next;
-        temp->next = nodeToDelete->next;
-        free(nodeToDelete);
+        Node* del = temp->next;
+        temp->next = del->next;
+
+        if (del == obj->tail)
+            obj->tail = temp;
+
+        free(del);
     }
 
     obj->size--;
 }
 
 
-/** Free the linked list */
 void myLinkedListFree(MyLinkedList* obj) {
-    Node* current = obj->head;
-    while (current != NULL) {
-        Node* next = current->next;
-        free(current);
-        current = next;
+    Node* curr = obj->head;
+    while (curr) {
+        Node* next = curr->next;
+        free(curr);
+        curr = next;
     }
     free(obj);
 }
+
+/** Free the linked list */
 
 /**
  * Your MyLinkedList struct will be instantiated and called as such:
