1912ca78869ea95ba4c21015ed9d12e86297d1c9
aggarwalshardul <aggarwalshardul@gmail.com>
2026-03-04 22:16:58 +0530

    Time: 6 ms (35.1%), Space: 19.8 MB (38.64%) - LeetHub

--- 0155-min-stack/0155-min-stack.c
+++ 0155-min-stack/0155-min-stack.c
@@ -1,44 +1,54 @@
 #include <stdlib.h>
 
 typedef struct {
-    int stack[10000];
-    int minStack[10000];
+    long *stack;
     int top;
+    long min;
 } MinStack;
 
 MinStack* minStackCreate() {
     MinStack* obj = (MinStack*)malloc(sizeof(MinStack));
+    obj->stack = (long*)malloc(sizeof(long) * 10000);
     obj->top = -1;
+    obj->min = 0;
     return obj;
 }
 
 void minStackPush(MinStack* obj, int val) {
-    obj->top++;
-    obj->stack[obj->top] = val;
+    if (obj->top == -1) {
+        obj->stack[++obj->top] = 0;
+        obj->min = val;
+    } else {
+        long diff = (long)val - obj->min;
+        obj->stack[++obj->top] = diff;
 
-    if (obj->top == 0)
-        obj->minStack[obj->top] = val;
-    else {
-        if (val < obj->minStack[obj->top - 1])
-            obj->minStack[obj->top] = val;
-        else
-            obj->minStack[obj->top] = obj->minStack[obj->top - 1];
+        if (diff < 0)
+            obj->min = val;
     }
 }
 
 void minStackPop(MinStack* obj) {
-    obj->top--;
+    long diff = obj->stack[obj->top--];
+
+    if (diff < 0)
+        obj->min = obj->min - diff;
 }
 
 int minStackTop(MinStack* obj) {
-    return obj->stack[obj->top];
+    long diff = obj->stack[obj->top];
+
+    if (diff > 0)
+        return obj->min + diff;
+    else
+        return obj->min;
 }
 
 int minStackGetMin(MinStack* obj) {
-    return obj->minStack[obj->top];
+    return obj->min;
 }
 
 void minStackFree(MinStack* obj) {
+    free(obj->stack);
     free(obj);
 }
 /**
