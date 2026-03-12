d655f18c791a05bb6c0808ad97772ffc6cf33bcd
aggarwalshardul <aggarwalshardul@gmail.com>
2026-03-12 17:55:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.c
+++ 0739-daily-temperatures/0739-daily-temperatures.c
@@ -5,24 +5,23 @@
 
 int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
     
-    int *answer = (int*)calloc(temperaturesSize, sizeof(int));
+    int *ans = (int*)calloc(temperaturesSize, sizeof(int));
     int *stack = (int*)malloc(sizeof(int) * temperaturesSize);
     
     int top = -1;
-    
-    for(int i = 0; i < temperaturesSize; i++){
-        
-        while(top != -1 && temperatures[i] > temperatures[stack[top]]){
-            
-            int index = stack[top--];
-            answer[index] = i - index;
+
+    for (int i = 0; i < temperaturesSize; i++) {
+
+        while (top >= 0 && temperatures[i] > temperatures[stack[top]]) {
+            int idx = stack[top--];
+            ans[idx] = i - idx;
         }
-        
+
         stack[++top] = i;
     }
-    
+
     *returnSize = temperaturesSize;
     free(stack);
-    
-    return answer;
+
+    return ans;
 }
