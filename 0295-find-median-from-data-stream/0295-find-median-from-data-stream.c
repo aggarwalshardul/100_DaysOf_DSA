415b5b65c7c7c8591ab65b28e63154b64d015aa7
aggarwalshardul <aggarwalshardul@gmail.com>
2026-03-14 21:14:59 +0530

    Time: 76 ms (49.62%), Space: 101.6 MB (20.3%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.c
@@ -0,0 +1,133 @@
+#include <stdlib.h>
+
+typedef struct {
+    int maxHeap[50005];
+    int minHeap[50005];
+    int maxSize;
+    int minSize;
+} MedianFinder;
+
+
+/* ---------- Heap Utilities ---------- */
+
+void swap(int *a,int *b){
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
+
+/* MAX HEAP */
+
+void maxPush(MedianFinder* obj,int val){
+    int i=obj->maxSize++;
+    obj->maxHeap[i]=val;
+
+    while(i>0){
+        int p=(i-1)/2;
+        if(obj->maxHeap[p]>=obj->maxHeap[i]) break;
+        swap(&obj->maxHeap[p],&obj->maxHeap[i]);
+        i=p;
+    }
+}
+
+int maxPop(MedianFinder* obj){
+    int top=obj->maxHeap[0];
+    obj->maxHeap[0]=obj->maxHeap[--obj->maxSize];
+
+    int i=0;
+
+    while(1){
+        int l=2*i+1;
+        int r=2*i+2;
+        int largest=i;
+
+        if(l<obj->maxSize && obj->maxHeap[l]>obj->maxHeap[largest])
+            largest=l;
+
+        if(r<obj->maxSize && obj->maxHeap[r]>obj->maxHeap[largest])
+            largest=r;
+
+        if(largest==i) break;
+
+        swap(&obj->maxHeap[i],&obj->maxHeap[largest]);
+        i=largest;
+    }
+
+    return top;
+}
+
+
+/* MIN HEAP */
+
+void minPush(MedianFinder* obj,int val){
+    int i=obj->minSize++;
+    obj->minHeap[i]=val;
+
+    while(i>0){
+        int p=(i-1)/2;
+        if(obj->minHeap[p]<=obj->minHeap[i]) break;
+        swap(&obj->minHeap[p],&obj->minHeap[i]);
+        i=p;
+    }
+}
+
+int minPop(MedianFinder* obj){
+    int top=obj->minHeap[0];
+    obj->minHeap[0]=obj->minHeap[--obj->minSize];
+
+    int i=0;
+
+    while(1){
+        int l=2*i+1;
+        int r=2*i+2;
+        int smallest=i;
+
+        if(l<obj->minSize && obj->minHeap[l]<obj->minHeap[smallest])
+            smallest=l;
+
+        if(r<obj->minSize && obj->minHeap[r]<obj->minHeap[smallest])
+            smallest=r;
+
+        if(smallest==i) break;
+
+        swap(&obj->minHeap[i],&obj->minHeap[smallest]);
+        i=smallest;
+    }
+
+    return top;
+}
+
+/* ---------- MedianFinder ---------- */
+
+MedianFinder* medianFinderCreate() {
+    MedianFinder* obj = malloc(sizeof(MedianFinder));
+    obj->maxSize=0;
+    obj->minSize=0;
+    return obj;
+}
+
+void medianFinderAddNum(MedianFinder* obj, int num) {
+
+    if(obj->maxSize==0 || num<=obj->maxHeap[0])
+        maxPush(obj,num);
+    else
+        minPush(obj,num);
+
+    if(obj->maxSize>obj->minSize+1)
+        minPush(obj,maxPop(obj));
+
+    if(obj->minSize>obj->maxSize)
+        maxPush(obj,minPop(obj));
+}
+
+double medianFinderFindMedian(MedianFinder* obj) {
+
+    if(obj->maxSize==obj->minSize)
+        return (obj->maxHeap[0]+obj->minHeap[0])/2.0;
+
+    return obj->maxHeap[0];
+}
+
+void medianFinderFree(MedianFinder* obj) {
+    free(obj);
+}
