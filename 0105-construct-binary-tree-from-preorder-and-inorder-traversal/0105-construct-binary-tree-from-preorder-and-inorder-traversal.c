646e6e713491dd67139a36c7b1dd70daae38ce6d
aggarwalshardul <aggarwalshardul@gmail.com>
2026-03-30 22:39:56 +0530

    Time: 3 ms (90.35%), Space: 15 MB (16.46%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.c
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+**/
+#include <stdlib.h>
+
+// Create new node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Find index in inorder
+int findIndex(int* inorder, int start, int end, int value) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+// Recursive function
+struct TreeNode* build(int* preorder, int* inorder,
+                       int inStart, int inEnd,
+                       int* preIndex) {
+
+    if (inStart > inEnd)
+        return NULL;
+
+    int rootVal = preorder[*preIndex];
+    (*preIndex)++;
+
+    struct TreeNode* root = newNode(rootVal);
+
+    if (inStart == inEnd)
+        return root;
+
+    int inIndex = findIndex(inorder, inStart, inEnd, rootVal);
+
+    root->left = build(preorder, inorder, inStart, inIndex - 1, preIndex);
+    root->right = build(preorder, inorder, inIndex + 1, inEnd, preIndex);
+
+    return root;
+}
+
+// Main function
+struct TreeNode* buildTree(int* preorder, int preorderSize,
+                          int* inorder, int inorderSize) {
+
+    int preIndex = 0;
+    return build(preorder, inorder, 0, inorderSize - 1, &preIndex);
+}
