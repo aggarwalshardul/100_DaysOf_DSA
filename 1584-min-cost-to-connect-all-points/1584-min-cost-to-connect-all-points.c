91b184f16d475eaa97576ced2fd1354737d046a3
aggarwalshardul <aggarwalshardul@gmail.com>
2026-04-12 22:12:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.c
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.c
@@ -2,10 +2,13 @@
 #include <limits.h>
 
 int minCostConnectPoints(int** points, int n, int* colSize) {
-    int *minDist = (int*)malloc(n * sizeof(int));
-    char *visited = (char*)calloc(n, sizeof(char)); // faster than int
-    
-    for(int i = 0; i < n; i++) minDist[i] = INT_MAX;
+    int minDist[n];
+    int visited[n];
+
+    for(int i = 0; i < n; i++) {
+        minDist[i] = INT_MAX;
+        visited[i] = 0;
+    }
 
     minDist[0] = 0;
     int result = 0;
@@ -13,7 +16,7 @@ int minCostConnectPoints(int** points, int n, int* colSize) {
     for(int i = 0; i < n; i++) {
         int u = -1;
 
-        // find minimum
+        // pick minimum
         for(int j = 0; j < n; j++) {
             if(!visited[j] && (u == -1 || minDist[j] < minDist[u])) {
                 u = j;
@@ -23,19 +26,11 @@ int minCostConnectPoints(int** points, int n, int* colSize) {
         visited[u] = 1;
         result += minDist[u];
 
-        int ux = points[u][0];
-        int uy = points[u][1];
-
         // update distances
         for(int v = 0; v < n; v++) {
             if(!visited[v]) {
-                int dx = ux - points[v][0];
-                if(dx < 0) dx = -dx;
-
-                int dy = uy - points[v][1];
-                if(dy < 0) dy = -dy;
-
-                int dist = dx + dy;
+                int dist = abs(points[u][0] - points[v][0]) +
+                           abs(points[u][1] - points[v][1]);
 
                 if(dist < minDist[v]) {
                     minDist[v] = dist;
@@ -44,7 +39,5 @@ int minCostConnectPoints(int** points, int n, int* colSize) {
         }
     }
 
-    free(minDist);
-    free(visited);
     return result;
 }
