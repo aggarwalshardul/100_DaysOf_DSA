b0dea4d980e33c1e33dbdd702f048bb678b1fafc
aggarwalshardul <aggarwalshardul@gmail.com>
2026-04-15 20:58:20 +0530

    Time: 0 ms (100%), Space: 10.6 MB (17.53%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.c
@@ -0,0 +1,44 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int parent[1001];
+
+// Find with path compression
+int find(int x) {
+    if (parent[x] != x)
+        parent[x] = find(parent[x]);
+    return parent[x];
+}
+
+// Union
+void unionSet(int a, int b) {
+    int pa = find(a);
+    int pb = find(b);
+    if (pa != pb)
+        parent[pa] = pb;
+}
+
+int* findRedundantConnection(int** edges, int edgesSize, int* edgesColSize, int* returnSize) {
+    for (int i = 1; i <= edgesSize; i++)
+        parent[i] = i;
+
+    int* result = (int*)malloc(2 * sizeof(int));
+
+    for (int i = 0; i < edgesSize; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        if (find(u) == find(v)) {
+            result[0] = u;
+            result[1] = v;
+            *returnSize = 2;
+            return result;
+        }
+        unionSet(u, v);
+    }
+
+    return NULL;
+}
